c96409e2c1d8c01ea958607e4e0a217196751f06
jerry <brownjer3@gmail.com>
2020-12-04 17:15:47 -0800

    commit tree 4fc853dd125a8d00034144313c485061f0c7c617

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,42 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  # code actions here!
+  get '/recipes' do 
+    @recipes = Recipe.all
+    erb :index
+  end
+
+  get '/recipes/new' do
+    erb :new
+  end 
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    erb :show
+  end
+
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find(params[:id])
+    erb :edit
+  end
+
+  post '/recipes' do 
+    r = Recipe.create(params)
+    redirect "/recipes/#{r.id}"
+  end
+
+  patch '/recipes/:id' do 
+    r = Recipe.find(params[:id])
+    r.update(params[:recipe])
+  
+    redirect "/recipes/#{r.id}"
+  end
+
+  delete '/recipes/:id' do
+    r = Recipe.find(params[:id])
+    r.destroy
+
+    redirect '/recipes'
+  end
 
 end
